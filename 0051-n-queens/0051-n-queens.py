4e999cd1dde50d70fc535cd84ddbbf053836e828
Harshita Srivastava <harshitaw02481@gmail.com>
2026-07-04 13:54:20 +0530

    Time: 4 ms (98.55%), Space: 19.8 MB (33.35%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,36 +1,25 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def check(col,row,board):
-            for i in range(col):
-                if board[row][i]=='Q':
-                    return False
-            i=row
-            j=col
-            while i>=0 and j>=0:
-                if board[i][j]=='Q':
-                    return False
-                i-=1
-                j-=1
-            i=row
-            j=col
-            while i<n and j>=0:
-                if board[i][j]=='Q':
-                    return False
-                i+=1
-                j-=1
-            return True
-        def bt(board,col,result):
+        def bt(col,result,board,crow,cud,cld):
             if col==n:
                 temp=["".join(row) for row in board]
                 result.append(temp)
-                return 
+                return
             for row in range(n):
-                if check(col,row,board):
+                if crow[row]==0 and cud[row+col]==0 and cld[row-col]==0:
                     board[row][col]='Q'
-                    bt(board,col+1,result)
+                    crow[row]=1
+                    cud[row+col]=1
+                    cld[row-col]=1
+                    bt(col+1,result,board,crow,cud,cld)
+                    crow[row]=0
+                    cud[row+col]=0
+                    cld[row-col]=0
                     board[row][col]='.'
-
-        board=[['.' for i in range(n)]for i in range(n)]
+        board=[['.' for i in range(n)] for i in range(n)]
+        crow=[0]*n
+        cud=[0]*(2*n-1)
+        cld=[0]*(2*n-1)
         result=[]
-        bt(board,0,result)
+        bt(0,result,board,crow,cud,cld)
         return result
