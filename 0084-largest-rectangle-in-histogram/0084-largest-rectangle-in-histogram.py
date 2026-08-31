9d9320bc4cf73839f4340726d44ca8674b61c72e
Harshita Srivastava <harshitaw02481@gmail.com>
2026-08-31 20:41:55 +0530

    Time: 113 ms (76.31%), Space: 30.6 MB (88.36%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,27 +1,19 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        def nse(arr):
-            stack=[]
-            nser=[-1]*len(arr)
-            for i in range(len(arr)-1,-1,-1):
-                while stack and arr[stack[-1]]>=arr[i]:
-                    stack.pop()
-                nser[i]=stack[-1] if stack else len(arr)
-                stack.append(i)
-            return nser
-        def pse(arr):
-            stack=[]
-            pser=[-1]*len(arr)
-            for i in range(len(arr)):
-                while stack and arr[stack[-1]]>arr[i]:
-                    stack.pop()
-                pser[i]=stack[-1] if stack else -1
-                stack.append(i)
-            return pser
-        nser=nse(heights)
-        pser=pse(heights)
+        stack=[]
         area=0
         for i in range(len(heights)):
-            curr=heights[i]*(nser[i]-pser[i]-1)
-            area=max(area,curr)
+            while stack and heights[stack[-1]]>=heights[i]:
+                nse=i
+                curr=heights[stack.pop()]
+                pse=stack[-1] if stack else -1
+                area=max(area,(curr*(nse-pse-1)))
+            stack.append(i)
+        while stack:
+            nse=len(heights)
+            curr=heights[stack.pop()]
+            pse=stack[-1] if stack else -1
+            area=max(area,curr*(nse-pse-1))
         return area
+
+
